8a7004f63ae0a015c5dd98343d6eba870052b1d3
svermeulen <sfvermeulen@gmail.com>
2014-12-18 00:59:02 -0400

    Fixed string subsequence matching

--- OmniSharp/AutoComplete/StringExtensions.cs
+++ OmniSharp/AutoComplete/StringExtensions.cs
@@ -11,14 +11,14 @@ namespace OmniSharp.AutoComplete
         }
 
         public static bool IsValidCompletionStartsWithExactCase(this string completion, string partial)
-		{
-             return completion.StartsWith(partial); 
-		}
+        {
+             return completion.StartsWith(partial);
+        }
 
         public static bool IsValidCompletionStartsWithIgnoreCase(this string completion, string partial)
-		{
-             return completion.ToLower().StartsWith(partial.ToLower()); 
-		}
+        {
+             return completion.ToLower().StartsWith(partial.ToLower());
+        }
 
         public static bool IsCamelCaseMatch(this string completion, string partial)
 
@@ -26,15 +26,43 @@ namespace OmniSharp.AutoComplete
             return new string(completion.Where(c => c >= 'A' && c <= 'Z').ToArray()).StartsWith(partial.ToUpper());
         }
 
-		public static bool IsSubsequenceMatch(this string completion, string partial)
-		{
-		    if (partial == string.Empty)
-		        return true;
+        public static bool IsSubsequenceMatch(this string completion, string partial)
+        {
+            if (partial == string.Empty)
+                return true;
 
-			var firstLetter = partial.ToLower()[0];
-			if(!(firstLetter >= 'a' && firstLetter <= 'z'))
-				return false;
-			return new string(completion.ToUpper().Intersect(partial.ToUpper()).ToArray()) == partial.ToUpper();
-		}
+            var firstLetter = partial.ToLower()[0];
+
+            if (!(firstLetter >= 'a' && firstLetter <= 'z'))
+                return false;
+
+            completion = completion.ToUpper();
+            partial = partial.ToUpper();
+
+            int index = 0;
+            foreach (char c in partial)
+            {
+                bool found = false;
+
+                while (index < completion.Length)
+                {
+                    if (completion[index] == c)
+                    {
+                        found = true;
+                        index++;
+                        break;
+                    }
+
+                    index++;
+                }
+
+                if (!found)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
